b269e1a3028e0ad452838a08f0ad888e15f64866
Vaifav SV <vaifav06ai@gmail.com>
2026-07-17 09:12:02 +0530

    drizzle relations added

--- drizzle/db/index.ts
+++ drizzle/db/index.ts
@@ -1,2 +1,4 @@
 export * from "@/drizzle/schema/products"
 export * from "@/drizzle/schema/users"
+export * from "@/drizzle/schema/pincodes"
+export * from "@/drizzle/schema/orders"
--- drizzle/relations/relations.ts
+++ drizzle/relations/relations.ts
@@ -1,59 +1,77 @@
 import { defineRelations } from "drizzle-orm";
 import * as schema from "../db/index";
 
-export const relations = defineRelations(schema, (relation) => {
-	return {
-		products: {
-			categories: relation.one.categories({
-				from: relation.products.categoryId,
-				to: relation.categories.id,
-			}),
-			productsToTags: relation.many.productsToTags(),
-			productsToIngredients: relation.many.productsToIngredients(),
-			productsToModifiers: relation.many.productsToModifiers(),
-			productsToPaymentMethods: relation.many.productsToPaymentMethods(),
-		},
-		categories: { products: relation.many.products() },
-		tags: { productsToTags: relation.many.productsToTags() },
-		modifiers: { productsToModifiers: relation.many.productsToModifiers() },
-		ingredients: { productsToIngredients: relation.many.productsToIngredients() },
-		paymentMethods: { productsToPaymentMethods: relation.many.productsToPaymentMethods() },
-		productsToTags: {
-			products: relation.one.products({
-				from: relation.productsToTags.productId,
-				to: relation.products.id,
-			}),
-			tags: relation.one.tags({ from: relation.productsToTags.tagId, to: relation.tags.id }),
-		},
-		productsToIngredients: {
-			products: relation.one.products({
-				from: relation.productsToIngredients.productId,
-				to: relation.products.id,
-			}),
-			ingredients: relation.one.ingredients({
-				from: relation.productsToIngredients.ingredientId,
-				to: relation.ingredients.id,
-			}),
-		},
-		productsToModifiers: {
-			products: relation.one.products({
-				from: relation.productsToModifiers.productId,
-				to: relation.products.id,
-			}),
-			modifiers: relation.one.modifiers({
-				from: relation.productsToModifiers.modifierId,
-				to: relation.modifiers.id,
-			}),
-		},
-		productsToPaymentMethods: {
-			products: relation.one.products({
-				from: relation.productsToPaymentMethods.productId,
-				to: relation.products.id,
-			}),
-			paymentMethods: relation.one.paymentMethods({
-				from: relation.productsToPaymentMethods.paymentMethodId,
-				to: relation.paymentMethods.id,
-			}),
-		},
-	};
-});
+export const relations = defineRelations(schema, (r) => ({
+	pincodes: {
+		profiles: r.many.profiles({
+			from: r.pincodes.pincode.through(r.addresses.pincode),
+			to: r.profiles.id.through(r.addresses.userId)
+		}),
+		orders: r.many.orders(),
+	},
+	profiles: {
+		pincodes: r.many.pincodes(),
+	},
+	orders: {
+		products: r.many.products({
+			from: r.orders.id.through(r.orderItems.orderId),
+			to: r.products.id.through(r.orderItems.productId)
+		}),
+		pincode: r.one.pincodes({
+			from: r.orders.addressPincode,
+			to: r.pincodes.pincode
+		}),
+	},
+	products: {
+		orders: r.many.orders(),
+		productImages: r.many.productImages(),
+		category: r.one.categories({
+			from: r.products.categoryId,
+			to: r.categories.id
+		}),
+		ingredients: r.many.ingredients(),
+		modifiers: r.many.modifiers(),
+		paymentMethods: r.many.paymentMethods(),
+		tags: r.many.tags({
+			from: r.products.id.through(r.productsToTags.productId),
+			to: r.tags.id.through(r.productsToTags.tagId)
+		}),
+		variants: r.many.variants(),
+	},
+	productImages: {
+		product: r.one.products({
+			from: r.productImages.productId,
+			to: r.products.id
+		}),
+	},
+	categories: {
+		products: r.many.products(),
+	},
+	ingredients: {
+		products: r.many.products({
+			from: r.ingredients.id.through(r.productsToIngredients.ingredientId),
+			to: r.products.id.through(r.productsToIngredients.productId)
+		}),
+	},
+	modifiers: {
+		products: r.many.products({
+			from: r.modifiers.id.through(r.productsToModifiers.modifierId),
+			to: r.products.id.through(r.productsToModifiers.productId)
+		}),
+	},
+	paymentMethods: {
+		products: r.many.products({
+			from: r.paymentMethods.id.through(r.productsToPaymentMethods.paymentMethodId),
+			to: r.products.id.through(r.productsToPaymentMethods.productId)
+		}),
+	},
+	tags: {
+		products: r.many.products(),
+	},
+	variants: {
+		product: r.one.products({
+			from: r.variants.productId,
+			to: r.products.id
+		}),
+	},
+}))
